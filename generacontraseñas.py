8bdce9a71913eb32a358dc743772f1bf8cf51d64
Setik <setikbloxoficial@gmail.com>
2025-01-16 16:11:41 -0600

    generacontraseñas.py

--- generacontraseñas.py
+++ generacontraseñas.py
@@ -2,7 +2,7 @@ import random
 
 Characters = ["+", "-", "*", "/", "^", "(", ")", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "&", "?", ">", "<", "=", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-longitud = int(input("Ingrese la longitud de la contraseña: "))
+longitud = (input("Ingrese la longitud de la contraseña: "))
 
 contraseña = longitud
 
